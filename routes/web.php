510ae105cf6a4117e2fbc9a31ac99c370df2a831
everton souza <everton.info10@gmail.com>
2025-08-12 23:09:37 -0300

    Fluxo de cadastro e login

--- routes/web.php
+++ routes/web.php
@@ -5,69 +5,72 @@ use App\Http\Controllers\UserController;
 use App\Models\Book;
 use App\Models\User;
 
-Route::get('/', function () {
+
+Route::middleware(['auth'])->group(function() {
+    Route::get('/', function () {
 
 
-    // Example of creating a new Book instance
-    // $post = new \App\Models\Book();
-    // $post->title = 'Laravel 10';
-    // $post->body = 'Laravel 12 is the latest version of the Laravel framework.';
-    // $post->save();
+        // Example of creating a new Book instance
+        // $post = new \App\Models\Book();
+        // $post->title = 'Laravel 10';
+        // $post->body = 'Laravel 12 is the latest version of the Laravel framework.';
+        // $post->save();
 
-    /// Example create method usage
-    // $post = \App\Models\Book::create([
-    //     'title' => 'Laravel 10',
-    //     'body' => 'Laravel 12 is the latest version of the Laravel framework.'
-    // ]);
-
-
-    // ------------------------
-
-    // $book = Book::find(1);
-
-    //$book = Book::where('id', 1)->first();
-
-    // $book = Book::where('title', 'LIKE', '%laravel%')->get();
-    // dd($book);
+        /// Example create method usage
+        // $post = \App\Models\Book::create([
+        //     'title' => 'Laravel 10',
+        //     'body' => 'Laravel 12 is the latest version of the Laravel framework.'
+        // ]);
 
 
-    // -------------------------
+        // ------------------------
 
-    // $input = [
-    //     'title' => "Meu novo titulo vindo do input", 
-    //     'body' => 'Meu novo body vindo do input'
-    // ];
+        // $book = Book::find(1);
 
-    // $book = Book::find(2);
-    // $book->fill($input);
-    // $book->save();
+        //$book = Book::where('id', 1)->first();
+
+        // $book = Book::where('title', 'LIKE', '%laravel%')->get();
+        // dd($book);
 
 
-    // -------------------------
-   
-    // $book = Book::find(3);
-    // $book->delete();
+        // -------------------------
 
-    // -------------------------
+        // $input = [
+        //     'title' => "Meu novo titulo vindo do input", 
+        //     'body' => 'Meu novo body vindo do input'
+        // ];
 
-    // 1 -> 1
-    // $user = User::find(1);
-    // $user = User::with('profile')->find(1);
-    // $user->profile()->create([
-    //     'type' => 'PJ', 
-    //     'document_number' => '9238748234'
-    // ]);
-    // dd($user);
+        // $book = Book::find(2);
+        // $book->fill($input);
+        // $book->save();
 
 
-    // 1 -> N
+        // -------------------------
+    
+        // $book = Book::find(3);
+        // $book->delete();
+
+        // -------------------------
+
+        // 1 -> 1
+        // $user = User::find(1);
+        // $user = User::with('profile')->find(1);
+        // $user->profile()->create([
+        //     'type' => 'PJ', 
+        //     'document_number' => '9238748234'
+        // ]);
+        // dd($user);
 
 
-    return view('welcome');
+        // 1 -> N
+
+
+        return view('home');
+    });
+
+    Route::get('admin/usuarios', [UserController::class, 'index'])->name('users.index');
+    Route::get('admin/usuarios/cadastrar', [UserController::class, 'create'])->name('users.store');
+    Route::post('admin/usuarios/cadastrar', [UserController::class, 'store'])->name('users.create');
+    Route::get('admin/usuarios/{id}', [UserController::class, 'show']);
+
 });
-
-Route::get('admin/usuarios', [UserController::class, 'index'])->name('users.index');
-Route::get('admin/usuarios/cadastrar', [UserController::class, 'create'])->name('users.store');
-Route::post('admin/usuarios/cadastrar', [UserController::class, 'store'])->name('users.create');
-
-Route::get('admin/usuarios/{id}', [UserController::class, 'show']);
